05778264423711c45ccf790c7eee4c3859f626fe
Manh Duc <phungmanhduc2209@gmail.com>
2026-07-10 10:01:41 +0700

    Fix production CORS

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.use(
       return callback(new Error(`CORS blocked: ${origin}`));
     },
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   })
 );
 // app.use(
